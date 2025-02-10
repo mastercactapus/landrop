63378ad9d2fb681eff34a6a48c8e0145033ffc61
Nathaniel Caza <mastercactapus@gmail.com>
2025-02-10 17:42:20 -0600

    add support for HTTPS with self-signed certificate generation

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/pion/logging v0.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.2.0 // indirect
--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
 	_ "embed"
 	"flag"
 	"log"
+	"math/big"
 	"net"
 	"net/http"
+	"net/url"
 	"path/filepath"
+	"strconv"
+	"sync"
+	"time"
 
+	"github.com/golang/groupcache/lru"
 	"github.com/mastercactapus/landrop/app"
 	"github.com/pion/mdns"
 	"golang.org/x/net/ipv4"
@@ -20,6 +31,9 @@ func main() {
 	dir := flag.String("dir", ".", "Directory to serve.")
 	uploads := flag.Bool("w", false, "Allow drag-and-drop uploads.")
 	scan := flag.Bool("s", false, "Scan file contents for description (requires `file` tool).")
+	https := flag.Bool("https", false, "Serve over HTTPS (if -cert and -key are omitted, will generate self-signed cert).")
+	cert := flag.String("cert", "", "Path to TLS certificate.")
+	key := flag.String("key", "", "Path to TLS key.")
 	flag.Parse()
 
 	mdnsAddr, err := net.ResolveUDPAddr("udp", mdns.DefaultAddress)
@@ -54,14 +68,88 @@ func main() {
 	if *uploads {
 		log.Println("Uploads enabled.")
 	}
-	log.Printf("Serving %s on http://%s:%d (http://%s)", absDir, *name, hl.Addr().(*net.TCPAddr).Port, hl.Addr())
 
-	err = http.Serve(hl, app.NewServer(app.Config{
-		Name:     *name,
-		Dir:      absDir,
-		Writable: *uploads,
-		Scan:     *scan,
-	}))
+	srv := &http.Server{
+		Handler: app.NewServer(app.Config{
+			Name:     *name,
+			Dir:      absDir,
+			Writable: *uploads,
+			Scan:     *scan,
+		}),
+	}
+	if *https && *cert == "" && *key == "" {
+		priv, err := rsa.GenerateKey(rand.Reader, 2048)
+		if err != nil {
+			panic(err)
+		}
+
+		l := lru.New(100)
+		var mx sync.Mutex
+		srv.TLSConfig = &tls.Config{
+			GetCertificate: func(hello *tls.ClientHelloInfo) (*tls.Certificate, error) {
+				c, ok := l.Get(hello.ServerName)
+				if ok {
+					return c.(*tls.Certificate), nil
+				}
+				mx.Lock()
+				defer mx.Unlock()
+
+				// Check again in case another goroutine set it.
+				c, ok = l.Get(hello.ServerName)
+				if ok {
+					return c.(*tls.Certificate), nil
+				}
+
+				now := time.Now()
+				template := &x509.Certificate{
+					SerialNumber: big.NewInt(now.Unix()),
+					Subject: pkix.Name{
+						CommonName:         hello.ServerName,
+						OrganizationalUnit: []string{"landrop"},
+					},
+					NotBefore: now,
+					NotAfter:  now.AddDate(0, 0, 1), // Valid for one day
+
+					BasicConstraintsValid: true,
+					IsCA:                  true,
+					ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+					KeyUsage: x509.KeyUsageKeyEncipherment |
+						x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+				}
+				cert, err := x509.CreateCertificate(rand.Reader, template, template,
+					priv.Public(), priv)
+				if err != nil {
+					return nil, err
+				}
+
+				var outCert tls.Certificate
+				outCert.Certificate = append(outCert.Certificate, cert)
+				outCert.PrivateKey = priv
+				l.Add(hello.ServerName, &outCert)
+				return &outCert, nil
+			},
+		}
+	}
+
+	u := url.URL{
+		Scheme: "http",
+		Host:   net.JoinHostPort(*name, strconv.Itoa(hl.Addr().(*net.TCPAddr).Port)),
+	}
+
+	if *https {
+		u.Scheme = "https"
+	}
+
+	nameURL := u.String()
+	u.Host = hl.Addr().String()
+	addrURL := u.String()
+
+	log.Printf("Serving %s on %s (%s)", absDir, nameURL, addrURL)
+	if *https {
+		err = srv.ServeTLS(hl, *cert, *key)
+	} else {
+		err = srv.Serve(hl)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
